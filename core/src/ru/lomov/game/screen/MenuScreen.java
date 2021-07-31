e3adb0f8734ad43b41667df598edfe551de991df
Torward <lomovfg@gmail.com>
2021-07-31 19:11:43 +1000

    1.Реализовано движение 2.Реализована остановка объекта в точке призыва.

--- core/src/ru/lomov/game/screen/MenuScreen.java
+++ core/src/ru/lomov/game/screen/MenuScreen.java
@@ -13,12 +13,15 @@ public class MenuScreen  extends BaseScreen {
     private Texture fon;
     private int x = 0;
     private int y = 0;
-    private int w = 256;
-    private int h = 256;
+    private int w = 128;
+    private int h = 128;
     private int fw = 600;
     private int fh = 600;
     private Vector2 pos;
     private Vector2 vel;
+    private Vector2 posA;
+    private Vector2 mov;
+    private Vector2 dir;
 
     @Override
     public void show() {
@@ -26,33 +29,33 @@ public class MenuScreen  extends BaseScreen {
         fon = new Texture("cosmos.jpg");
         img = new Texture("ufo .png");
         pos = new Vector2();
-        vel = new Vector2(1,1);
+        posA = new Vector2();
+        mov = new Vector2(0,0);
+        vel = new Vector2();
+        dir = new Vector2();
 
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-      // pos.y++;
-       // pos.x++;
-        if (h <= 256) {
-            w--;
-            h--;
-
-        }
-        if (h <= 0) {
-            w = 2;
-            h = 2;
-        }
         batch.enableBlending();
         ScreenUtils.clear(1, 0, 0, 1);
         batch.begin();
         batch.draw(fon, 0, 0,fw,fh);
         batch.draw(img, pos.x, pos.y, w, h);
         batch.end();
-        pos.add(vel);
-    }
+        if(pos.dst2(posA)> vel.dst2(dir)){
+            pos.add(vel);
+        } else {
+            pos.set(posA);
 
+        }
+
+
+
+
+    }
     @Override
     public void dispose() {
         super.dispose();
@@ -62,13 +65,23 @@ public class MenuScreen  extends BaseScreen {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        pos.set(screenX, Gdx.graphics.getHeight()-screenY);
+        posA.set(screenX-64, Gdx.graphics.getHeight()-screenY-64);
+       dir.set(posA).sub(pos).nor();
+       vel = new Vector2(dir).scl(5);
+
+
         return super.touchDown(screenX, screenY, pointer, button);
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
         pos.set(screenX, Gdx.graphics.getHeight()-screenY);
+        dir.set(posA).sub(pos).nor();
+        vel = new Vector2(dir).scl(5);
         return super.touchDragged(screenX, screenY, pointer);
+    }
+    public void resetPos(Vector2 pos){
+        this.pos = pos;
+        pos.set(0,0);
     }
 }
